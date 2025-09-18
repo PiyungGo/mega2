e1181901e21367f6163296fd6182fd4668204309
NarinoYoshi <pgsensai2125@gmail.com>
2025-09-18 22:03:54 +0700

    Update 0.2

--- Utolis.gd
+++ Utolis.gd
@@ -1,4 +1,3 @@
 extends Sprite2D
 
 const Board_Size = 8
-#aaasss
--- board.gd
+++ board.gd
@@ -1,11 +1,14 @@
 extends Sprite2D
 
 const BOARD_SIZE = 8
+const cell_width = 18
 
-const good_bishop = "res://Asset_UWU/S__5128198.png"
-const center_bishop = "res://Asset_UWU/S__5128204.png"
-const hacker_ishop = "res://Asset_UWU/S__5128209.png"
+const texture_holder = preload("res://texture_holder.tscn")
 
+const good_bishop = preload("res://Asset_UWU/S__5128198.png")
+const call_bishop = preload("res://Asset_UWU/S__5128204.png")
+const hacker_bishop = preload("res://Asset_UWU/S__5128209.png")
+const police_bishop = preload("res://Asset_UWU/character/S__5128217_0.png")
 
 
 @onready var pieces = $Pieces
@@ -15,13 +18,31 @@ const hacker_ishop = "res://Asset_UWU/S__5128209.png"
 var board : Array
 var good : bool
 var state : bool
-var move = {}
+var move = []
 var selected_piece : Vector2
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready() :
+	board.append([1,0,0,0,0,0,0,2])
+	board.append([0,0,0,0,0,0,0,0])
+	board.append([0,0,0,0,0,0,0,0])
+	board.append([0,0,0,0,0,0,0,0])
+	board.append([0,0,0,0,0,0,0,0])
+	board.append([0,0,0,0,0,0,0,0])
+	board.append([0,0,0,0,0,0,0,0])
+	board.append([3,0,0,0,0,0,0,4])
+	
+	display_board()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func display_board():
+	for i in BOARD_SIZE:
+		for j in BOARD_SIZE:
+			var holder = texture_holder.instantiate()
+			pieces.add_child(holder)
+			holder.global_position = Vector2 (j * cell_width + (cell_width / 2), -i * cell_width - (cell_width / 2))
+			
+			match board[i][j]:
+				0: holder.texture = null
+				1: holder.texture = good_bishop
+				2: holder.texture = call_bishop
+				3: holder.texture = hacker_bishop
+				4: holder.texture = police_bishop

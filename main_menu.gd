00678e4f3c594a1c57b8d5b24925fed431dc5d33
Teerawas777 <terawas.tayasakul@gmail.com>
2025-09-19 17:47:14 +0700

    Main Menu

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_Start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Creat_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Setting_pressed() -> void:
+	pass # Replace with function body.
